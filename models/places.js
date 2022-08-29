afa4dca6c430bdfa68f559a38278305480cdd41c
AriesRedWar <philipjbowman5@gmail.com>
2022-08-29 09:58:44 -0400

    New places can be added

---
+++ models/places.js
@@ -0,0 +1,13 @@
+module.exports = [{
+    name: 'H-Thai-ML',
+    city: 'Seattle',
+    state: 'WA',
+    cuisines: 'Thai, Pan-Asian',
+    pic: '/images/Rustic Restaurant.jpg'
+}, {
+    name: 'Coding Cat Cafe',
+    city: 'Phoenix',
+    state: 'AZ',
+    cuisines: 'Coffee, Bakery',
+    pic: '/images/Rustic Restaurant 2.jpg'
+}]
