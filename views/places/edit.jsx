e0a15c43d7a8208b76f4d9a70d2433c824bb91fc
AriesRedWar <philipjbowman5@gmail.com>
2022-09-01 18:14:29 -0400

    added the edit functionality

--- views/places/edit.jsx
+++ views/places/edit.jsx
@@ -1,33 +1,50 @@
 const React = require("react");
-const Def = require("../default.jsx");
+const Def = require("../default");
+
 
 function edit_form(data) {
   return (
     <Def>
       <main id="New-Form">
-        <h1>Edit Place</h1>
-        <form method="POST" action={`/places/${data.place.id}$_method=PUT`}>
+        <h1>Edit {data.place.name}</h1>
+        <form method="POST" action={`/places/${data.place.id}?_method=PUT`} >
           <div className="form-group">
             <label htmlFor="name">Place Name</label>
             <input
               className="form-control"
               id="name"
               name="name"
-              value={data.place.name}
+              defaultValue={data.place.name}
               required
             />
           </div>
           <div className="form-group">
             <label htmlFor="pic">Place Picture</label>
-            <input type="url" className="form-control" id="pic" name="pic" />
+            <input
+              /*type="url"*/
+              className="form-control"
+              id="pic"
+              name="pic"
+              defaultValue={data.place.pic}
+            />
           </div>
           <div className="form-group">
             <label htmlFor="city">City</label>
-            <input className="form-control" id="city" name="city" />
+            <input
+              className="form-control"
+              id="city"
+              name="city"
+              defaultValue={data.place.city}
+            />
           </div>
           <div className="form-group">
             <label htmlFor="state">State</label>
-            <select className="form-control" id="state" name="state">
+            <select
+              className="form-control"
+              id="state"
+              name="state"
+              defaultValue={data.place.state}
+            >
               <option value="AL">Alabama</option>
               <option value="AK">Alaska</option>
               <option value="AZ">Arizona</option>
@@ -85,11 +102,15 @@ function edit_form(data) {
               className="form-control"
               id="cuisines"
               name="cuisines"
+              defaultValue={data.place.cuisines}
               required
             />
           </div>
-          <input className="btn btn-primary" type="submit" value="Update Place" />
-        </form>{" "}
+          <input
+            className="btn btn-primary"
+            type="submit"
+          />
+        </form>
       </main>
     </Def>
   );
