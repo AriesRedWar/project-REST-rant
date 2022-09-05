d7829bbe6e85a9952b41d314b7fbf59ceb39c96d
AriesRedWar <philipjbowman5@gmail.com>
2022-09-05 18:46:37 -0400

    added places schema for the show view spiced up the description with the data from the place.

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -6,26 +6,22 @@ function show(data) {
     <Def>
       <main>
         <body>
-          <div id="mainPlace">
+          <div className="row"  id="mainPlace">
+          <div className="col-sm-6">
             <img id="placeimg" src={data.place.pic} alt={data.place.name} />
-            <div className="row">
+            <h3>
+              Located in {data.place.city}, {data.place.state}
+            </h3>
+            </div>
+            <div className="col-sm-6">
               <h1>{data.place.name}</h1>
-              <h3 id="Rate"> Rating</h3>
-              <h4>
-                <p> Not Rated</p>
-              </h4>
-
-              <h3 id="Desc"> Description</h3>
-              <h4>
-                <p>
-                  {" "}
-                  Located in {data.place.city}, {data.place.state} and serving{" "}
-                  {data.place.cuisines}
-                </p>
-              </h4>
+              <h3 id="Rate">Rating</h3>
+              <h4>Not Rated</h4>
+              <h3 id="Desc">Description</h3>
+              <h4>{data.place.showEstablished()}</h4>
+              <h5>Serving {data.place.cuisines}</h5>
             </div>
           </div>
-
           <h3> Comments</h3>
           <h4>
             <p> No comments yet!</p>
