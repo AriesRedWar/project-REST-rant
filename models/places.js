fdfed5cb7c816da6488714049bc77491212e1f59
AriesRedWar <philipjbowman5@gmail.com>
2022-09-05 17:59:31 -0400

    changed the index to be .js file in modules folder and to be running on mongo instead of internal js

--- models/places.js
+++ models/places.js
@@ -1,18 +1,12 @@
-module.exports = [
-  {
-    id: "0",
-    name: "H-Thai-ML",
-    city: "Seattle",
-    state: "WA",
-    cuisines: "Thai, Pan-Asian",
-    pic: "/images/Rustic Restaurant.jpg",
-  },
-  {
-    id: "1",
-    name: "Coding Cat Cafe",
-    city: "Phoenix",
-    state: "AZ",
-    cuisines: "Coffee, Bakery",
-    pic: "/images/Rustic Restaurant 2.jpg",
-  },
-];
+const mongoose = require('mongoose')
+
+const placeSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  pic: String,
+  cuisines: { type: String, required: true },
+  city: { type: String, default: 'Anytown' },
+  state: { type: String, default: 'USA' },
+  founded: Number
+})
+
+module.exports = mongoose.model('Place', placeSchema)
