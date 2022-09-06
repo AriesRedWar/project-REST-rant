e45db47293d2451f26b6b88aacb591b2df4b1026
AriesRedWar <philipjbowman5@gmail.com>
2022-09-06 17:26:56 -0400

    Added option to make a comment for a place.

--- controllers/places.js
+++ controllers/places.js
@@ -55,20 +55,49 @@ router.put("/:id", (req, res) => {
   res.send("PUT /places/:id stub");
 });
 
-router.delete("/:id", (req, res) => {
-  res.send("DELETE /places/:id stub");
-});
+router.delete('/:id', (req, res) => {
+  db.Place.findByIdAndDelete(req.params.id)
+  .then(place => {
+      res.redirect('/places')
+  })
+  .catch(err => {
+      console.log('err', err)
+      res.render('error404')
+  })
+})
+
 
 router.get("/:id/edit", (req, res) => {
   res.send("GET edit form stub");
 });
 
-router.post("/:id/rant", (req, res) => {
-  res.send("GET /places/:id/rant stub");
-});
+router.post('/:id/comments', (req, res) => {
+  // console.log(req.body)
+  req.body.rant = req.body.rant ? true : false
+  db.Place.findById(req.params.id)
+  .then(place => {
+      db.Comment.create(req.body)
+      .then(comment => {
+          place.comments.push(comment.id)
+          place.save()
+          .then(() => {
+              res.redirect(`/places/${req.params.id}`)
+          })
+      })
+      .catch(err => {
+          res.render('error404')
+      })
+  })
+  .catch(err => {
+      res.render('error404')
+  })
 
-router.delete("/:id/rant/:rantId", (req, res) => {
-  res.send("GET /places/:id/rant/:rantId stub");
+  // res.send('GET /places/:id/comments stub')
+})
+
+
+router.delete("/:id/rant/:commentId", (req, res) => {
+  res.send("GET /places/:id/comment/:commentId stub");
 });
 
 module.exports = router;
--- views/places/show.jsx
+++ views/places/show.jsx
@@ -2,20 +2,20 @@ const React = require("react");
 const Def = require("../default");
 
 function show(data) {
-  let comments = (<h4 className="inactive">No Comments yet!</h4>)
-  if (data.place.comments.length) { 
-    comments = data.place.comments.map(c => {
-    return (
-      <div className="border">
-        <h2 className="rant">{c.rant ? 'Rant!' : 'Rave!'}</h2>
-        <h4>{c.content}</h4>
-            <h4>
-              <stong>- {c.author}</stong>
-            </h4>
-            <h4>Rating: {c.stars}</h4>
-      </div>
-    )
-    })
+  let comments = <h4 className="inactive">No Comments yet!</h4>;
+  if (data.place.comments.length) {
+    comments = data.place.comments.map((c) => {
+      return (
+        <div className="border">
+          <h2 className="rant">{c.rant ? "Rant! 😡" : "Rave! 😍"}</h2>
+          <h4>{c.content}</h4>
+          <h4>
+            <stong>- {c.author}</stong>
+          </h4>
+          <h4>Rating: {c.stars}</h4>
+        </div>
+      );
+    });
   }
   return (
     <Def>
@@ -35,19 +35,84 @@ function show(data) {
               <h3 id="Desc">Description</h3>
               <h4>{data.place.showEstablished()}</h4>
               <h5>Serving {data.place.cuisines}</h5>
+              <div id="ah">
+                <form id="btn" method="POST">
+                  {" "}
+                  <a
+                    type="click"
+                    href={`/places/${data.place.id}/edit`}
+                    className="btn btn-warning"
+                  >
+                    Edit
+                  </a>
+                </form>
+                <form
+                  id="btn"
+                  method="POST"
+                  action={`/places/${data.place.id}?_method=DELETE`}
+                >
+                  <button type="submit" className="btn btn-danger">
+                    Delete
+                  </button>
+                </form>
+              </div>
             </div>
           </div>
-          <h3> Comments</h3>
-          {comments}
+          <div>
+            <h3> Comments</h3>
+            {comments}
+          </div>
+          <div>
+            <h1>Got Your Own Rant or Rave?</h1>
+            <form
+              action={`/places/${data.place.id}/comments`}
+              method="POST"
+            >
+              <div className="row">
+                <div className="form-group col-sm-12">
+                  <label htmlFor="content">Content</label>
+                  <textarea
+                    id="content"
+                    name="content"
+                    className="form-control"
+                  ></textarea>
+                </div>
+              </div>
+              <div className="row">
+                <div className="form-group col-sm-4">
+                  <label htmlFor="author">Author</label>
+                  <input id="author" name="author" className="form-control" />
+                </div>
+                <div className="form-group col-sm-4">
+                  <label htmlFor="stars">Star Rating</label>
+                  <input
+                    type="range"
+                    step="0.5"
+                    min="1"
+                    max="5"
+                    id="stars"
+                    name="stars"
+                    className="form-control"
+                  />
+                </div>
+                <div className="form-group col-sm-2">
+                  <label htmlFor="rant">Rant?</label>
+                  <input
+                    type="checkbox"
+                    id="rant"
+                    name="rant"
+                    className="form-control"
+                  />
+                </div>
+              </div>
+              <input
+                type="submit"
+                className="btn btn-primary"
+                value="Add Comment"
+              />
+            </form>
+          </div>
         </body>
-        <a href={`/places/${data.id}/edit`} className="btn btn-warning">
-          Edit
-        </a>
-        <form method="POST" action={`/places/${data.id}?_method=DELETE`}>
-          <button type="submit" className="btn btn-danger">
-            Delete
-          </button>
-        </form>
       </main>
     </Def>
   );
